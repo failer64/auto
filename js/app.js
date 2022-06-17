3285e7eadcd10cb45a37656b9d9153ddb72e6312
failer64 <80625459+failer64@users.noreply.github.com>
2022-06-17 23:23:07 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -383,13 +383,6 @@
                 document.documentElement.classList.add(className);
             }));
         }
-        function addLoadedClass() {
-            window.addEventListener("load", (function() {
-                setTimeout((function() {
-                    document.documentElement.classList.add("loaded");
-                }), 0);
-            }));
-        }
         let bodyLockStatus = true;
         let bodyLockToggle = (delay = 500) => {
             if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
@@ -444,6 +437,11 @@
             setTimeout((() => {
                 if (window.FLS) console.log(message);
             }), 0);
+        }
+        function uniqArray(array) {
+            return array.filter((function(item, index, self) {
+                return self.indexOf(item) === index;
+            }));
         }
         let gotoblock_gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 0) => {
             const targetBlockElement = document.querySelector(targetBlock);
@@ -649,6 +647,129 @@
             class_loaded: "_lazy-loaded",
             use_native: true
         });
+        class ScrollWatcher {
+            constructor(props) {
+                let defaultConfig = {
+                    logging: true
+                };
+                this.config = Object.assign(defaultConfig, props);
+                this.observer;
+                !document.documentElement.classList.contains("watcher") ? this.scrollWatcherRun() : null;
+            }
+            scrollWatcherUpdate() {
+                this.scrollWatcherRun();
+            }
+            scrollWatcherRun() {
+                document.documentElement.classList.add("watcher");
+                this.scrollWatcherConstructor(document.querySelectorAll("[data-watch]"));
+            }
+            scrollWatcherConstructor(items) {
+                if (items.length) {
+                    this.scrollWatcherLogging(`Проснулся, слежу за объектами (${items.length})...`);
+                    let uniqParams = uniqArray(Array.from(items).map((function(item) {
+                        return `${item.dataset.watchRoot ? item.dataset.watchRoot : null}|${item.dataset.watchMargin ? item.dataset.watchMargin : "0px"}|${item.dataset.watchThreshold ? item.dataset.watchThreshold : 0}`;
+                    })));
+                    uniqParams.forEach((uniqParam => {
+                        let uniqParamArray = uniqParam.split("|");
+                        let paramsWatch = {
+                            root: uniqParamArray[0],
+                            margin: uniqParamArray[1],
+                            threshold: uniqParamArray[2]
+                        };
+                        let groupItems = Array.from(items).filter((function(item) {
+                            let watchRoot = item.dataset.watchRoot ? item.dataset.watchRoot : null;
+                            let watchMargin = item.dataset.watchMargin ? item.dataset.watchMargin : "0px";
+                            let watchThreshold = item.dataset.watchThreshold ? item.dataset.watchThreshold : 0;
+                            if (String(watchRoot) === paramsWatch.root && String(watchMargin) === paramsWatch.margin && String(watchThreshold) === paramsWatch.threshold) return item;
+                        }));
+                        let configWatcher = this.getScrollWatcherConfig(paramsWatch);
+                        this.scrollWatcherInit(groupItems, configWatcher);
+                    }));
+                } else this.scrollWatcherLogging("Сплю, нет объектов для слежения. ZzzZZzz");
+            }
+            getScrollWatcherConfig(paramsWatch) {
+                let configWatcher = {};
+                if (document.querySelector(paramsWatch.root)) configWatcher.root = document.querySelector(paramsWatch.root); else if ("null" !== paramsWatch.root) this.scrollWatcherLogging(`Эмм... родительского объекта ${paramsWatch.root} нет на странице`);
+                configWatcher.rootMargin = paramsWatch.margin;
+                if (paramsWatch.margin.indexOf("px") < 0 && paramsWatch.margin.indexOf("%") < 0) {
+                    this.scrollWatcherLogging(`Ой ой, настройку data-watch-margin нужно задавать в PX или %`);
+                    return;
+                }
+                if ("prx" === paramsWatch.threshold) {
+                    paramsWatch.threshold = [];
+                    for (let i = 0; i <= 1; i += .005) paramsWatch.threshold.push(i);
+                } else paramsWatch.threshold = paramsWatch.threshold.split(",");
+                configWatcher.threshold = paramsWatch.threshold;
+                return configWatcher;
+            }
+            scrollWatcherCreate(configWatcher) {
+                this.observer = new IntersectionObserver(((entries, observer) => {
+                    entries.forEach((entry => {
+                        this.scrollWatcherCallback(entry, observer);
+                    }));
+                }), configWatcher);
+            }
+            scrollWatcherInit(items, configWatcher) {
+                this.scrollWatcherCreate(configWatcher);
+                items.forEach((item => this.observer.observe(item)));
+            }
+            scrollWatcherIntersecting(entry, targetElement) {
+                if (entry.isIntersecting) {
+                    !targetElement.classList.contains("_watcher-view") ? targetElement.classList.add("_watcher-view") : null;
+                    this.scrollWatcherLogging(`Я вижу ${targetElement.classList}, добавил класс _watcher-view`);
+                } else {
+                    targetElement.classList.contains("_watcher-view") ? targetElement.classList.remove("_watcher-view") : null;
+                    this.scrollWatcherLogging(`Я не вижу ${targetElement.classList}, убрал класс _watcher-view`);
+                }
+            }
+            scrollWatcherOff(targetElement, observer) {
+                observer.unobserve(targetElement);
+                this.scrollWatcherLogging(`Я перестал следить за ${targetElement.classList}`);
+            }
+            scrollWatcherLogging(message) {
+                this.config.logging ? FLS(`[Наблюдатель]: ${message}`) : null;
+            }
+            scrollWatcherCallback(entry, observer) {
+                const targetElement = entry.target;
+                this.scrollWatcherIntersecting(entry, targetElement);
+                targetElement.hasAttribute("data-watch-once") && entry.isIntersecting ? this.scrollWatcherOff(targetElement, observer) : null;
+                document.dispatchEvent(new CustomEvent("watcherCallback", {
+                    detail: {
+                        entry
+                    }
+                }));
+                if (entry.isIntersecting) {
+                    var elem = document.createElement("script");
+                    elem.src = "https://api-maps.yandex.ru/2.1?apikey=8d7bd03a-f132-4234-ac7b-1e031670c1a1&lang=ru_RU";
+                    document.getElementsByTagName("body")[0].appendChild(elem);
+                    setTimeout((function() {
+                        ymaps.ready(init);
+                    }), 1e3);
+                    function init(ymaps) {
+                        let myMap = new ymaps.Map("map", {
+                            controls: [],
+                            center: [ 59.953928, 30.371139 ],
+                            zoom: 10
+                        });
+                        let myPlacemark = new ymaps.Placemark([ 59.9588, 30.474933 ], {
+                            balloonContentHeader: "ST TUNING",
+                            balloonContentBody: "Ириновский проспект 24к.",
+                            balloonContentFooter: "<a  href = 'tel:88129050600' > 8 (812) 905-06-00 </a >",
+                            hasBalloon: true,
+                            hideIconOnBalloonOpen: true
+                        }, {
+                            iconLayout: "default#imageWithContent",
+                            iconImageHref: "../../img/map.png",
+                            iconImageSize: [ 40, 40 ],
+                            iconImageOffset: [ -20, -20 ],
+                            iconContentOffset: [ 0, 0 ]
+                        });
+                        myMap.geoObjects.add(myPlacemark);
+                    }
+                }
+            }
+        }
+        flsModules.watcher = new ScrollWatcher({});
         let addWindowScrollEvent = false;
         setTimeout((() => {
             if (addWindowScrollEvent) {
@@ -754,39 +875,8 @@
         };
         const da = new DynamicAdapt("max");
         da.init();
-        function mapAdd() {
-            var script = document.createElement("script");
-            script.src = "https://api-maps.yandex.ru/2.1?apikey=8d7bd03a-f132-4234-ac7b-1e031670c1a1&lang=ru_RU";
-            document.head.appendChild(script);
-            setTimeout((function() {
-                ymaps.ready(init);
-            }), 2e3);
-        }
-        if (document.querySelector("#map")) mapAdd();
-        function init(ymaps) {
-            let myMap = new ymaps.Map("map", {
-                controls: [],
-                center: [ 59.953928, 30.371139 ],
-                zoom: 10
-            });
-            let myPlacemark = new ymaps.Placemark([ 59.9588, 30.474933 ], {
-                balloonContentHeader: "ST TUNING",
-                balloonContentBody: "Ириновский проспект 24к.",
-                balloonContentFooter: "<a  href = 'tel:88129050600' > 8 (812) 905-06-00 </a >",
-                hasBalloon: true,
-                hideIconOnBalloonOpen: true
-            }, {
-                iconLayout: "default#imageWithContent",
-                iconImageHref: "../img/map.png",
-                iconImageSize: [ 40, 40 ],
-                iconImageOffset: [ -20, -20 ],
-                iconContentOffset: [ 0, 0 ]
-            });
-            myMap.geoObjects.add(myPlacemark);
-        }
         window["FLS"] = false;
         isWebp();
-        addLoadedClass();
         menuInit();
         formFieldsInit({
             viewPass: false
